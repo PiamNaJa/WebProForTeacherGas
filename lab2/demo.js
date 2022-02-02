1e7304ed3a7081dcc562a50a8758625c6887cc88
“PiamNaJa“ <peumza1150@gmail.com>
2022-02-02 15:45:03 +0700

    PP

---
+++ lab2/demo.js
@@ -0,0 +1,4 @@
+let Cha = prompt("What Is Your Favorite Charactor");
+console.log(Cha + " Is Your Favortie Charactor");
+let Color = prompt("What Is The Main Color Of " + Cha +"?");
+console.log(Color + " Is Main Color Of " + Cha);
---
+++ lab2/guessnumber.js
@@ -0,0 +1,27 @@
+let number = Math.floor(Math.random() * 101);
+let frontnum = 0;
+let lastnum = 100;
+let player;
+while(true)
+{
+    player = prompt("Guess The Number Between "+ frontnum +" - "+ lastnum);
+    player = parseInt(player);
+    if(player===number)
+    {
+        alert("It Correct!\nSecert Number Is " + number);
+        break;
+    }
+    else if(player > frontnum && player < number)
+    {
+        frontnum = player;
+    }
+    else if(player < lastnum && player > number)
+    {
+        lastnum = player;
+    }
+    else if(player > lastnum)
+    {
+        alert("YOU LOSE!\nSecert Number Is " + number);
+        break;
+    }
+}
